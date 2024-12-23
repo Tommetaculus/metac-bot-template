1e3d306ef2906ce099f39cbc9d62d11554ab5970
Tom Liptay <118371148+Tommetaculus@users.noreply.github.com>
2024-12-23 10:34:28 -0500

    Update GET_NEWS to True

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ SUBMIT_PREDICTION = True  # set to True to publish your predictions to Metaculus
 USE_EXAMPLE_QUESTIONS = False  # set to True to forecast example questions rather than the tournament questions
 NUM_RUNS_PER_QUESTION = 5  # The median forecast is taken between NUM_RUNS_PER_QUESTION runs
 SKIP_PREVIOUSLY_FORECASTED_QUESTIONS = True
-GET_NEWS = False  # set to True to enable AskNews after entering ASKNEWS secrets
+GET_NEWS = True  # set to True to enable AskNews after entering ASKNEWS secrets
 
 # Environment variables
 METACULUS_TOKEN = os.getenv("METACULUS_TOKEN")
